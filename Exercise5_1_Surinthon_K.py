1ad57f88189aeac7a43b78f6546cd13ba59a50be
wootkarawoot <53505266+wootkarawoot@users.noreply.github.com>
2020-04-28 15:00:13 +0700

    Create Exercise5_1_Surinthon_K.py

---
+++ Exercise5_1_Surinthon_K.py
@@ -0,0 +1,7 @@
+InputOne = int(input('1โปรดระบุตัวเลข'))
+InputTwo = int(input('2โปรดระบุตัวเลข'))
+
+print(InputOne,"+", InputTwo ,"=", InputOne+ InputTwo)
+print(InputOne,"-", InputTwo ,"=", InputOne- InputTwo)
+print(InputOne,"*", InputTwo ,"=", InputOne* InputTwo)
+print(InputOne,"/", InputTwo ,"=", InputOne/ InputTwo)
